3efa7ecd07403ce09206582021693ec3242d1691
Jonathan Birge <birge@mit.edu>
2019-01-15 19:27:28 -0500

    random pin handling

--- app.py
+++ app.py
@@ -1,27 +1,31 @@
 import os
-import os.path
+from random import randint
 from flask import Flask, flash, request, redirect, url_for
 from flask import send_from_directory, render_template, session
 from flask_autoindex import AutoIndex
 from werkzeug.utils import secure_filename
 
-UPLOAD_FOLDER = '/app/uploads'
+UPLOAD_BASE = '/app/files/'
 ALLOWED_EXTENSIONS = set(['jpg', 'png', 'jpeg', 'gif'])
-SECRET_KEY = 'viadelamesatemecula'
+SECRET_KEY = 'viadelamesatemeculaca'
+PIN_DIGITS = 10
 
 app = Flask(__name__)
 files_index = AutoIndex(app, os.path.curdir + '/files', add_url_rules=False)
-app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.config['UPLOAD_BASE'] = UPLOAD_BASE
 app.config['MAX_CONTENT_LENGTH'] = 4 * 1024 * 1024
 app.secret_key = SECRET_KEY
 
 @app.route('/')
 def index():
-    return 'Main page coming soon...'
+    return 'Main page coming soon!'
 
 def allowed_file(filename):
     return '.' in filename and \
            filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+def random_pin():
+    return str(randint(10**(PIN_DIGITS - 1), 10**PIN_DIGITS - 1))
 
 @app.route('/upload', methods=['GET', 'POST'])
 def upload_file():
@@ -34,11 +38,11 @@ def upload_file():
         else:
             file = request.files['file']
             if not allowed_file(file.filename):
-                flash('Bad file name!')
+                flash('Bad file type')
                 problems += 1
         userpin = request.form['pin']
         if len(userpin) < 6:
-            flash('PIN is too short!')
+            flash('PIN is too short')
             problems += 1
         else:
             session['pin'] = userpin
@@ -47,9 +51,16 @@ def upload_file():
         # handle request
         filename = secure_filename(file.filename)
         # TODO fullpath = 'example.com/files' + userpin + '/' + filename
-        file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+        fullpath = os.path.join(app.config['UPLOAD_BASE'], userpin)
+        try:
+            os.mkdir(fullpath)
+        except:
+            pass  # we don't care!
+        file.save(os.path.join(fullpath, filename))
         return render_template('success.html', pin=userpin, filename=filename)
     else: # if GET
+        if not 'pin' in session:
+            session['pin'] = random_pin() # TODO make random hash
         return render_template('upload.html')
 
 @app.route('/files')
