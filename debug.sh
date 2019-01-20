d42509a1a99021e34470b01d451c43ebc3041094
Jonathan Birge <birge@mit.edu>
2019-01-20 01:16:25 -0500

    debug.sh cleans up

--- debug.sh
+++ debug.sh
@@ -3,4 +3,5 @@ COMPOSE_FILE=docker-compose-debug.yml
 docker-compose -f $COMPOSE_FILE down
 docker-compose -f $COMPOSE_FILE build
 docker-compose -f $COMPOSE_FILE up
+docker-compose -f $COMPOSE_FILE down
 rm -rf __pycache__
